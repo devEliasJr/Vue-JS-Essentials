be60eb89d68a00c5552f4639ad8d2ab707388f89
devEliasJr <eliasengmecanica@gmail.com>
2023-10-20 12:00:58 -0300

    refactor: Removed Option Api and moved style to APP

--- 2_Vue_PP/github-user-data/src/App.vue
+++ 2_Vue_PP/github-user-data/src/App.vue
@@ -1,17 +1,97 @@
-<script>
-import OptionsApi from './components/OptionsApi.vue'
+<script setup>
+// import OptionsApi from './components/OptionsApi.vue'
 import CompositionApi from './components/CompositionApi.vue';
-export default {
-  components: {
-    OptionsApi,
-    CompositionApi
-  }
-}
+
+// CHAMADA Para o uso com Option API
+// export default {
+//   components: {
+//     OptionsApi,
+//   }
+// }
+
 </script>
 
 <template>
-  <OptionsApi />
+  <!-- <OptionsApi /> -->
   <CompositionApi />
 </template>
 
-<style></style>
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background-color: #1c1a1d;
+  color: #e5e5e5;
+}
+
+#app {
+  font-family: "Ubuntu", sans-serif;
+  min-height: 100vh;
+  max-width: 40rem;
+  margin: 0 auto;
+  text-align: center;
+}
+
+img {
+  border: 1px solid #e5e5e5;
+  border-radius: 999px;
+  display: block;
+  margin: 1rem auto;
+  width: 8rem;
+  height: 8rem;
+}
+
+h1,
+h2 {
+  color: #f64348;
+  margin: 1 rem auto 0.25rem;
+}
+
+h3 {
+  margin: 1 rem auto 0.25rem;
+}
+
+span {
+  display: block;
+  margin: 1rem auto;
+}
+
+input,
+button {
+  max-width: 20rem;
+  margin: 1rem auto;
+}
+
+input {
+  background-color: #1c1a1d;
+  border: 1px solid #f64348;
+  border-radius: 0.25rem;
+  color: #e5e5e5;
+  margin: 1rem 1rem 1rem 0;
+  padding: 1rem;
+}
+
+button {
+  background-color: #f64348;
+  border: unset;
+  padding: .8rem;
+  border-radius: 0.25rem;
+  color: #1c1a1d;
+  font-size: 1rem;
+  font-weight: 700;
+  text-transform: uppercase;
+}
+
+button:hover {
+  cursor: pointer;
+  filter: brightness(0.95);
+}
+
+a {
+  color: #f64348;
+}
+</style>
--- 2_Vue_PP/github-user-data/src/components/CompositionApi.vue
+++ 2_Vue_PP/github-user-data/src/components/CompositionApi.vue
@@ -2,15 +2,21 @@
 import { reactive } from 'vue';
 
 const state = reactive({
-  login: "eliasDev",
+  login: "devEliasJr",
   name: "Elias",
   bio: "...",
   company: "EliasDev",
   avatar_url: "http://unsplash.it/256",
   searchInput: "",
+  repos: [],
 })
 
 async function fetchGithubUser() {
+
+  if (state.searchInput === "") {
+    return
+  }
+
   const res = await fetch(`https://api.github.com/users/${state.searchInput}`);
   const { login, name, bio, company, avatar_url } = await res.json();
 
@@ -19,15 +25,37 @@ async function fetchGithubUser() {
   state.company = company;
   state.bio = bio;
   state.avatar_url = avatar_url;
+
+  fetchUserRepositories(login);
+
 }
+
+async function fetchUserRepositories(username) {
+
+  const res = await fetch(`https://api.github.com/users/${username}/repos`);
+  const repos = await res.json();
+
+  state.repos = repos;
+}
+
 </script>
 
 <template>
+  <h1>GitHub User Data</h1>
   <input type="text" v-model="state.searchInput">
   <button v-on:click="fetchGithubUser">Carregar Usuario</button>
-  <img v-bind:src="state.avatar_url">
-  <strong>@{{ state.login }}</strong>
-  <h1>{{ state.name }}</h1>
-  <h2>{{ state.company }}</h2>
-  <span>{{ state.bio }}</span>
+  <div v-if="state.login !== ''">
+    <img v-bind:src="state.avatar_url">
+    <strong>@{{ state.login }}</strong>
+    <h1>{{ state.name }}</h1>
+    <h2>{{ state.company }}</h2>
+    <span>{{ state.bio }}</span>
+  </div>
+  <section v-if="state.repos.length > 0">
+    <article v-for="repo of state.repos" :key="repo.id">
+      <h3>{{ repo.full_name }}</h3>
+      <p>{{ repo.description }}</p>
+      <a :href="repo.html_url" target="_blank">Ver no Github</a>
+    </article>
+  </section>
 </template>
